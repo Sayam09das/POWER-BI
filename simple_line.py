bc36357632729472b559becc971ba51ac3701283
Sayam Das <154095595+Sayam09das@users.noreply.github.com>
2024-09-19 18:42:18 +0530

    Add files via upload

---
+++ simple_line.py
@@ -0,0 +1,9 @@
+# Create a simple line plot Matplotlib ND PYTHON. given the list X and Y where X=[1,2,3,4,5,6] and y=[30,80,40.20.100,50] what will the resulting plot look like when you run the code.
+
+import matplotlib.pyplot as plt
+
+X = [1, 2, 3, 4, 5, 6]
+Y = [30, 80, 40, 20, 100, 50]
+
+plt.plot(X, Y)
+plt.show()
